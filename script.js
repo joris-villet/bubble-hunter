ec19a6b7b6f9d0f808d80e1ece8a6f3a90db1741
joris villet <jorisvillet@gmail.com>
2020-11-21 13:56:08 +0100

    relooking des bubbles

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
    const displayGame = document.createElement('div');
    body.appendChild(displayGame)
 
-   const bubbleSpeed = () => Math.round(Math.random() * (20 - 8) + 8);
+   const bubbleSpeed = () => Math.round(Math.random() * (8 - 5) + 5);
 
    // Créer une bulle
    const createBubble = () => {
@@ -14,10 +14,11 @@ window.addEventListener('DOMContentLoaded', () => {
       const bubble = document.createElement('div');
       bubble.classList.add('bubble');
       displayGame.appendChild(bubble);
-      let randomBubbleSize = Math.round(Math.random() * (80 - 15) + 15);
-      let randomBubblePosBottom = Math.round(Math.random() * (1200 - 100) + 100);
+      let randomBubbleSize = Math.round(Math.random() * (100 - 30) + 30);
+      let randomBubblePosBottom = Math.round(Math.random() * (900 - 100) + 100);
       bubble.style.setProperty('height', randomBubbleSize + 'px');
       bubble.style.setProperty('width', randomBubbleSize + 'px');
+      bubble.style.setProperty('--size', randomBubbleSize + 'px');
       bubble.style.setProperty('--bottom', randomBubblePosBottom + 'px');
       bubble.style.setProperty('--left', randomBubblePosBottom + 'px');
       bubble.style.setProperty('animation-duration', bubbleSpeed() + 's');
