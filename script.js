a3b5fd51275e95b82c662a183bf0fdac7f8c30e3
joris villet <jorisvillet@gmail.com>
2020-11-21 10:17:18 +0100

    test: taille aléatoire de la bulle

--- script.js
+++ script.js
@@ -2,16 +2,27 @@ window.addEventListener('DOMContentLoaded', () => {
    
    console.info("document chargé");
 
+   const body = document.querySelector('body');
+   const bubble = document.createElement('div');
+
+   const randomBubbleSize = Math.round(Math.random() * (80 - 10) + 10);
+   console.log(randomBubbleSize)
+
    const setup = () => {
-      const bubble = document.createElement('div');
       bubble.classList.add('bubble');
-      document.querySelector('body').appendChild(bubble);
+      body.appendChild(bubble);
    }
 
    setup()
 
+   const btn = document.createElement('button');
+   btn.textContent = "button"
+   body.appendChild(btn);
 
-
+   btn.addEventListener('click', () => {
+      bubble.style.setProperty('height', randomBubbleSize + 'px');
+      bubble.style.setProperty('width', randomBubbleSize + 'px');
+   });
 
 
 
